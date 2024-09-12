18fb295f360fe9ac342ae231b55791365ab62fdb
Ona0nline <onasihle123@gmail.com>
2024-09-12 19:37:08 +0200

    Include build directory for deployment

---
+++ build/precache-manifest.34208d1d9115c04c13145881fccee007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50a43a6842fe120d42b5cebffc0c58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71d2cdc7b2bb9cfbb854",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ce66e20ee239cdac693",
+    "url": "/static/js/2.0b996866.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/static/js/2.0b996866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5e4b9560d03b5065a9",
+    "url": "/static/js/3.34006c6a.chunk.js"
+  },
+  {
+    "revision": "71d2cdc7b2bb9cfbb854",
+    "url": "/static/js/main.e9b8fbe8.chunk.js"
+  },
+  {
+    "revision": "59ec7d4f03505d4981b9",
+    "url": "/static/js/runtime-main.92347928.js"
+  }
+]);
